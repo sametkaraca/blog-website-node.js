d07138d7280a4885fc636dc1ab7aa85f506f2370
Samet Karaca <sametkaraca@Samets-MacBook-Pro.local>
2020-04-13 23:21:04 +0200

    Essetials are ready.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
+const app = express()
+const _ = require('lodash')
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const app = express()
-
 let posts = []
 
 //**ANCHOR MIDDLEWARES**//
@@ -18,16 +17,24 @@ app.use(express.static("public"))
 
 //**ANCHOR ROUTERS**//
 app.get('/', (req,res)=>{
+
   res.render('home',{
     homeStartingContent: homeStartingContent,
     posts: posts
   })
 })
 
-
 app.get('/about', (req,res)=>{res.render('about.ejs', {aboutContent})})
 app.get('/contact', (req,res)=>{res.render('contact.ejs', {contactContent})})
-app.get('/compose',(req,res)=>{res.render('compose.ejs')})
+app.get('/compose', (req,res)=>{res.render('compose.ejs')})
+
+app.get('/posts/:post', (req,res)=>{
+    const requestedTitle= _.lowerCase(req.params.post)
+    posts.forEach(post=> {
+      const storedTitle = _.lowerCase(post.title)
+      storedTitle === requestedTitle && res.render('post.ejs', {title: post.title, content: post.content })
+    }) 
+})
 
 //**ANCHOR POST REQS**//
 app.post('/compose',(req,res)=>{
@@ -42,7 +49,5 @@ app.post('/compose',(req,res)=>{
   res.redirect('/')
   
 })
-
-
 
 app.listen(3000, ()=>console.log("Server started on port 3000"))
